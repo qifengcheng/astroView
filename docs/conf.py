7d00e9e4e7138498bfe17c5e46c9fe33f15be61f
Qifeng Cheng_Gitkraken <chengqifeng2020@outlook.com>
2025-08-06 11:57:54 -0500

    documentation html config

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../astroView/'))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -9,11 +12,12 @@
 project = 'astroView'
 copyright = '2025, Qifeng Cheng'
 author = 'Qifeng Cheng'
+root_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -23,5 +27,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
